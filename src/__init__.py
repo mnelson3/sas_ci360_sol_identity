8b244c5576f4532a0d8a52d0b5e971f0382141bd
Mark Nelson <mark.a.nelson@outlook.com>
2024-04-02 22:39:08 +0000

    Initial commit

---
+++ src/__init__.py
@@ -0,0 +1,17 @@
+#! /usr/local/bin/python3
+# -*- mode: python ; coding: utf-8 -*-
+
+from sasci360apicore import communication
+from sasci360apicore import connection
+from sasci360apicore import data
+from sasci360apicore import encryption
+from sasci360apicore import listener
+from sasci360apicore import logger
+from sasci360apicore import reporter
+from sasci360apicore import scheduler
+
+from sasci360apiscim import base
+from sasci360apiscim import groups
+from sasci360apiscim import roles
+from sasci360apiscim import schemas
+from sasci360apiscim import users
